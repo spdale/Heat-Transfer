5bc48b8ffae2bd72e59151b8fab110198d6bb639
Sam Dale <sdale28@users.noreply.github.com>
2019-10-15 10:07:39 -0400

    Temperature labels done right.

--- Problem Set 3/old-2.py
+++ Problem Set 3/old-2.py
@@ -30,21 +30,21 @@ for j in range(1, (height - 1)):
 
 heatmap = plt.pcolor(data)
 
-plt.text(0.5, -0.02, "T = " + str(T_alpha) + "\N{DEGREE SIGN}C",
+plt.text(0.5, -0.05, "T = " + str(T_alpha) + "\N{DEGREE SIGN}C",
          horizontalalignment='center',
-         verticalalignment='top',
+         verticalalignment='center',
          rotation=0,
          clip_on=False,
          transform=plt.gca().transAxes)
-plt.text(0, 0.5, "T = " + str(T_bravo) + "\N{DEGREE SIGN}C",
-         horizontalalignment='right',
+plt.text(-0.05, 0.5, "T = " + str(T_bravo) + "\N{DEGREE SIGN}C",
+         horizontalalignment='left',
          verticalalignment='center',
          rotation=90,
          clip_on=False,
          transform=plt.gca().transAxes)
-plt.text(0.5, 1, "T = " + str(T_charlie) + "\N{DEGREE SIGN}C",
+plt.text(0.5, 1.02, "T = " + str(T_charlie) + "\N{DEGREE SIGN}C",
          horizontalalignment='center',
-         verticalalignment='bottom',
+         verticalalignment='center',
          rotation=0,
          clip_on=False,
          transform=plt.gca().transAxes)
@@ -55,7 +55,33 @@ plt.text(1, 0.5, "T = " + str(T_delta) + "\N{DEGREE SIGN}C",
          clip_on=False,
          transform=plt.gca().transAxes)
 
+# plt.text(1.02, 0.5, r"\bf{level set} $\phi$", {'color': 'C2', 'fontsize': 20},
+#          horizontalalignment='left',
+#          verticalalignment='center',
+#          rotation=90,
+#          clip_on=False,
+#          transform=plt.gca().transAxes)
+
 plt.axis("off")
+# ax = plt.axes()
+# ax.xaxis.set_ticks_position('none')
+# ax.yaxis.set_ticks_position('none')
+# ax.yaxis.set_major_locator(plt.NullLocator())
+# ax.xaxis.set_major_formatter(plt.NullFormatter())
+# ax.set_xlabel("T = " + str(T_alpha) + "\N{DEGREE SIGN}C")
+# ax.set_ylabel("T = " + str(T_bravo) + "\N{DEGREE SIGN}C")
+
+# ax2 = ax.twinx()
+# ax2.yaxis.set_label_position('right')
+# ax2.yaxis.set_ticks_position('none')
+# ax2.yaxis.set_major_locator(plt.NullLocator())
+# ax2.set_ylabel("T = " + str(T_delta) + "\N{DEGREE SIGN}C")
+
+# ax3 = ax.twiny()
+# ax3.xaxis.set_label_position('top')
+# ax3.xaxis.set_ticks_position('none')
+# ax3.xaxis.set_major_formatter(plt.NullFormatter())
+# ax3.set_xlabel("T = " + str(T_charlie) + "\N{DEGREE SIGN}C")
 
 plt.xlim(0, width)
 plt.ylim(0, height)
