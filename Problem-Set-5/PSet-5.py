86b9cadbdd4510e54575493f362a2c6b0bb58c5c
Sam Dale <sdale28@users.noreply.github.com>
2019-11-18 22:33:59 -0500

    Figured out m,n confusion.

--- Problem-Set-5/PSet-5.py
+++ Problem-Set-5/PSet-5.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+fileName = "Problem-Set-5"
+
 # Grid points:
 height = 30
 width = 30
 
+# Constants
+rho = 3000  # kg/m^3
+c = 840     # J/(kg*C)
+h = 28      # W/(m^2*C)  Convective Heat Transfer Coefficient
+k = 5.2     # W/(m*C)    Thermal Conductivity
 
 # Make set temperatures on fixed positions
 T_alpha   = 0    # (bottom boundary temperature)
@@ -12,67 +19,72 @@ T_bravo   = 40   # (left boundary temperature)
 T_charlie = 100  # (top boundary temperature)
 T_delta   = 100  # (right boundary temperature)
 
+T_initial = 15
+T_right = 38
+
+
 # Initialize matrix of zeros for that size
 # Note: index 0,0 is bottom left
-default_temp = (max(T_alpha, T_bravo, T_charlie, T_delta) + min(T_alpha, T_bravo, T_charlie, T_delta)) / 2
-data = np.zeros((height, width)) + default_temp
+#default_temp = (max(T_alpha, T_bravo, T_charlie, T_delta) + min(T_alpha, T_bravo, T_charlie, T_delta)) / 2
+#data = np.zeros((height, width)) + T_initial
+
+data = np.zeros((width, height)) + T_initial
+
+for j in range(height):
+    data[(width - 1), j] = T_right
+
+
+# data = [[0, 1, 2, 3, 4],
+#         [5, 6, 7, 8, 9]]
+
+# # for j in range(height):
+# #     for i in range(width):
+# #         print(data[i,j], end = ' ')
+# #     print("")
+
+# # print(np.flip(data))
+# print(data)
+# print(np.rot90(data))
+
+# data = np.rot90(data)
 
 # Set boundary conditions
-for i in range(width):
-    data[0, i] = T_alpha
-    data[(height - 1), i] = T_charlie
-for j in range(1, (height - 1)):
-    data[j, 0] = T_bravo
-    data[j, (width - 1)] = T_delta
-
-error_flag = True
-error_limit = 1e-4
-while error_flag:
-    large_error_term_found = False
-
-    # Gauss-Seidel Iteration
-    for n in range(1, (height - 1)):
-        for m in range(1, (width - 1)): 
-            data_old = data[n, m]
-            data[n, m] = 0.25 * (data[(n + 1), m] + data[(n - 1), m] + data[n, (m + 1)] + data[n, (m - 1)])
-
-            if not large_error_term_found:
-                error_term = abs(data[n, m] - data_old) / data_old
-                if (error_term <= error_limit):
-                    error_flag = False
-                else:
-                    error_flag = True
-                    large_error_term_found = True
-
-#print(data)
-
-fig1 = plt.figure(1)
-x = np.linspace(0, (width / height), width)
-index = np.ceil(height / 2)
-y = data[index.astype(int), :]
-plt.plot(x, y)
-plt.xlabel(r'$\mathrm{Position\ Along\ Width\ (Normalized\ to\ \frac{Width}{Height})}$')
-plt.ylabel("Temperature (\N{DEGREE SIGN}C)")
-plt.title("Temperature Along Vertically Centered Horizontal Path of Data")
-plt.savefig("Problem-Set-3/images/pset-3-figure-1.png")
-plt.show()
-
-fig2 = plt.figure(2)
-x = np.linspace(0, 1, height)
-index = np.ceil(width / 2)
-y = data[:, index.astype(int)]
-plt.plot(x, y)
-plt.xlabel(r'$\mathrm{Position\ Along\ Height\ (Normalized)}$')
-plt.ylabel("Temperature (\N{DEGREE SIGN}C)")
-plt.title("Temperature Along Horizontally Centered Vertical Path of Data")
-plt.savefig("Problem-Set-3/images/pset-3-figure-2.png")
-plt.show()
+# for i in range(width):
+#     data[i, 0] = T_alpha
+#     data[i, (height - 1)] = T_charlie
+# for j in range(1, (height - 1)):
+#     data[0, j] = T_bravo
+#     data[(width - 1), j] = T_delta
 
 
-fig3 = plt.figure(3)
+# error_flag = True
+# error_limit = 1e-4
+# while error_flag:
+#     large_error_term_found = False
+
+#     # Gauss-Seidel Iteration
+#     for n in range(1, (height - 1)):
+#         for m in range(1, (width - 1)): 
+#             data_old = data[n, m]
+#             data[n, m] = 0.25 * (data[(n + 1), m] + data[(n - 1), m] + data[n, (m + 1)] + data[n, (m - 1)])
+
+#             if not large_error_term_found:
+#                 error_term = abs(data[n, m] - data_old) / data_old
+#                 if (error_term <= error_limit):
+#                     error_flag = False
+#                 else:
+#                     error_flag = True
+#                     large_error_term_found = True
+
+print(np.rot90(data))
+
+data_printable = np.flipud(np.rot90(data))
+
+figNum = 1
+plt.figure(figNum)
 plt.axes().set_aspect('equal')
 plt.style.use('classic')
-heatmap = plt.pcolor(data)
+heatmap = plt.pcolor(data_printable)
 
 plt.text(0.5, -0.02, "T = " + str(T_alpha) + "\N{DEGREE SIGN}C",
          horizontalalignment='center',
@@ -108,5 +120,5 @@ cbar = plt.colorbar(heatmap)
 cbar.set_label("Temperature (\N{DEGREE SIGN}C)")
 plt.clim(0, 100)
 
-plt.savefig("Problem-Set-3/images/pset-3-figure-3.png")
+plt.savefig(fileName + "/images/" + fileName + "-" + str(figNum) + ".png")
 plt.show()
